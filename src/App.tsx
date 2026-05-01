ec0429cb8338886e749899301205c4634e313159
higorxi <higorgiovane7@gmail.com>
2026-05-01 17:07:53 -0300

    Add Chakra UI and theming support with color mode functionality
    - Updated package.json to include Chakra UI dependencies and next-themes for theming.
    - Configured TypeScript paths in tsconfig.app.json for easier imports.
    - Enhanced Vite configuration to support TypeScript path resolution.
    - Introduced ColorModeProvider and related components for managing light/dark themes.
    - Updated App component to wrap routes with the new Provider for theme context.
    - Added Toaster and Tooltip components for improved UI feedback and interactions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { AppRoutes } from './routes/app.routes';
+import { Provider } from './components/ui/provider';
+
 
 function App() {
   return (
+    <Provider>
     <BrowserRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
+    </Provider>
   );
 }
 
